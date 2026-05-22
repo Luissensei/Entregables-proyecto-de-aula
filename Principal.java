cd101252a6e0cc1e96ca763f3f0340f62b42a9a8
Luissensei <cantillol145@gmail.com>
2026-05-22 09:09:35 -0500

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,15 @@
+package papeleria;
+
+import papeleria.gui.VentanaLogin;
+import javax.swing.SwingUtilities;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new VentanaLogin().setVisible(true);
+            }
+        });
+    }
+}
